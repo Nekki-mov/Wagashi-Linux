966f184c1c72f5e9c32a2a3f99f2e3fabf9d9112
Nekki-mov <nborrego2002@gmail.com>
2026-06-30 14:49:49 +0200

    Fix wallpaper definitivo via wagashi-firstrun (ported from Sayuri), dark mode default, GPU driver autodetect, Broadcom sleep hook

--- profiledef.sh
+++ profiledef.sh
@@ -23,6 +23,7 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/hikaru"]="0:0:755"
   ["/usr/local/bin/dango"]="0:0:755"
+  ["/usr/local/bin/wagashi-firstrun"]="0:0:755"
   ["/usr/local/bin/hikaru-gui"]="0:0:755"
   ["/root/.config/autostart/hikaru.desktop"]="0:0:644"
   ["/root/Desktop/hikaru.desktop"]="0:0:755"
